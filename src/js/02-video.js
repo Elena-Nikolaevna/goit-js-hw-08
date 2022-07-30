37e1fc644e4c378e4d1241f0cfe84a56c72c54dc
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-07-30 20:15:30 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const videoplayerCurrentTime = 'videoplayer-current-time';
+console.log(videoplayerCurrentTime);
+const iframe = document.querySelector('#vimeo-player');
+console.log(iframe);
+const getSavedTime = localStorage.getItem(videoplayerCurrentTime);
+console.log(getSavedTime)
+const player = new Player(iframe);
+console.log(player)
+
+if (getSavedTime) {
+    player.setCurrentTime(getSavedTime);
+  }
+  
+  player.on('timeupdate', throttle(onTimeUpdate, 1000));
+  
+  function onTimeUpdate(time) {
+    let savedPausedPlayerTime = time.seconds;
+    localStorage.setItem(videoplayerCurrentTime, savedPausedPlayerTime);
+  }
