a47caacde7022f2a8bf6670f2ba60b3d5e401130
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-07-31 02:33:18 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+import throttle from 'lodash.throttle';
 
+const FORM_KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onformData, 500));
+
+const formData = {};
+
+function onFormSubmit(evt) {
+  console.log(JSON.parse(localStorage.getItem(FORM_KEY)));
+  evt.preventDefault(); // отправка формы
+  evt.currentTarget.reset(); // очищаем форму
+  localStorage.removeItem(FORM_KEY);
+}
+
+function onformData(evt) {
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem(FORM_KEY, JSON.stringify(formData));
+}
+
+function dataLocalStorage() {
+  const data = JSON.parse(localStorage.getItem(FORM_KEY));
+  const email = document.querySelector('.feedback-form input');
+  const message = document.querySelector('.feedback-form textarea');
+  if (data) {
+    email.value = data.email;
+    message.value = data.message;
+  }
+};
