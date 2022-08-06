19f8ae5c45ba2279c6afa1faea843f54edc4354c
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-08-06 13:57:29 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,67 +1,28 @@
 import throttle from 'lodash.throttle';
 
-const FORM_KEY = 'feedback-form-state';
-
-const formData = {};
-
-const refs = {
-  form: document.querySelector('.feedback-form'),
-  textarea: document.querySelector('.feedback-form textarea'),
-};
-refs.form.addEventListener('submit', onFormSubmit);
-refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
-
-refs.form.addEventListener('input', e => {
-  //console.log(e.target.name);
-  //console.log(e.target.value);
-  formData[e.target.name] = e.target.value;
-  console.log(formData)
-});
-
-populateTextarea();
-
-function onFormSubmit(evt) {
-  evt.preventDefault();
-  console.log('URA');
-  evt.currentTarget.reset();
-  localStorage.removeItem('FORM_KEY');
-}
-function onTextareaInput(evt) {
-  const message = evt.target.value;
-
-  localStorage.setItem('FORM_KEY', message);
-}
-function populateTextarea() {
-  const savedMessage = localStorage.getItem('FORM_KEY');
-  if (savedMessage) {
-    //console.log(savedMessage);
-    refs.textarea.value = savedMessage;
-  }
-}
-
-/* const FORM_KEY = 'feedback-form-state';
+const FORM_KEY = 'feedback-form-state'; //
 
 const form = document.querySelector('.feedback-form');
-form.addEventListener('submit', onFormSubmit);
-form.addEventListener('input', throttle(onformData, 500));
+form.addEventListener('submit', onFormSubmit); //?
+form.addEventListener('input', throttle(onFormData, 500)); //?
 
 const formData = {};
 
-function onFormSubmit(evt) {
-  const formData = new FormData(form);
-  formData.forEach((value, name) => console.log(`${name}: ${value}`));
-  //console.log(JSON.parse(localStorage.getItem(FORM_KEY)));
-  evt.preventDefault(); // отправка формы
-  evt.currentTarget.reset(); // очищаем форму
-  localStorage.removeItem(FORM_KEY);
-}
-
-function onformData(evt) {
+function onFormData(evt) {
   formData[evt.target.name] = evt.target.value;
+
   localStorage.setItem(FORM_KEY, JSON.stringify(formData));
 }
 
-function dataLocalStorage() {
+function onFormSubmit(evt) {
+  console.log(JSON.parse(localStorage.getItem(FORM_KEY)));
+  evt.preventDefault();
+  //console.log('URA');
+  evt.currentTarget.reset();
+  localStorage.removeItem(FORM_KEY);
+}
+
+(function dataFromLocalStorage() {
   const data = JSON.parse(localStorage.getItem(FORM_KEY));
   const email = document.querySelector('.feedback-form input');
   const message = document.querySelector('.feedback-form textarea');
@@ -69,5 +30,4 @@ function dataLocalStorage() {
     email.value = data.email;
     message.value = data.message;
   }
-}
- */
+})();
