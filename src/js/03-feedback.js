b7a52180f0c7018646b513ba19ee0044e8ae60b7
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-08-06 22:32:21 +0300

    Исправление после возврата дз

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,31 +3,38 @@ import throttle from 'lodash.throttle';
 const FORM_KEY = 'feedback-form-state'; //
 
 const form = document.querySelector('.feedback-form');
+
+initForm();
+
 form.addEventListener('submit', onFormSubmit); //?
 form.addEventListener('input', throttle(onFormData, 500)); //?
 
-const formData = {};
-
-function onFormData(evt) {
-  formData[evt.target.name] = evt.target.value;
-
-  localStorage.setItem(FORM_KEY, JSON.stringify(formData));
-}
-
 function onFormSubmit(evt) {
-  console.log(JSON.parse(localStorage.getItem(FORM_KEY)));
+  //console.log(JSON.parse(localStorage.getItem(FORM_KEY)));
   evt.preventDefault();
+  const formData = new FormData(form);
+  formData.forEach((value, name) => console.log(value, name));
   //console.log('URA');
   evt.currentTarget.reset();
   localStorage.removeItem(FORM_KEY);
 }
 
-(function dataFromLocalStorage() {
-  const data = JSON.parse(localStorage.getItem(FORM_KEY));
-  const email = document.querySelector('.feedback-form input');
-  const message = document.querySelector('.feedback-form textarea');
-  if (data) {
-    email.value = data.email;
-    message.value = data.message;
+function onFormData(evt) {
+  let constantFilter = localStorage.getItem(FORM_KEY);
+  constantFilter = constantFilter ? JSON.parse(constantFilter) : {};
+
+  constantFilter[evt.target.name] = evt.target.value;
+
+  localStorage.setItem(FORM_KEY, JSON.stringify(constantFilter));
+}
+
+function initForm() {
+  let constantFilter = localStorage.getItem(FORM_KEY);
+
+  if (constantFilter) {
+    constantFilter = JSON.parse(constantFilter);
+    Object.entries(constantFilter).forEach(([name, value]) => {
+      form.elements[name].value = value;
+    });
   }
-})();
+}
